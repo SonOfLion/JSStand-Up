32b67e9c0409d560c4162dd90157fb10a6c564af
SonOfLion <syplusplus@gmail.com>
2020-05-13 10:57:19 +0300

    2

--- JSLoop/index.js
+++ JSLoop/index.js
@@ -3,13 +3,13 @@ let n = 40;
 
 let result = 0;
 
-for (let i = m; m <= n; i++) {
+for (let i = m; i <= n; i++) {
     if (i % 5 === 0) {
         console.log("4 is called for number: " + i);
         console.log(i);
         continue;
     }
-    if (i & 2 === 0 && i % 4 === 0) {
+    if (i % 2 === 0 && i % 4 === 0) {
         console.log("1 is called for number: " + i);
         result += i;
         continue;
