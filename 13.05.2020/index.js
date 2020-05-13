cef93503c9ae00760f0eba40797d5b70ee05d774
SonOfLion <syplusplus@gmail.com>
2020-05-13 10:33:16 +0300

    2

---
+++ 13.05.2020/index.js
@@ -0,0 +1,12 @@
+let n = 20;
+let sum = '';
+let i = 0;
+
+while (i <= n) {
+
+    console.log("Loop is Called");
+
+    sum += i;
+    i++;
+}
+console.log(sum);
---
+++ JSLoop/index.js
@@ -0,0 +1,29 @@
+let n = 10;
+let m = 40;
+
+let result;
+
+for (let i = m; m <= n; i++) {
+    if (i % 5 === 0) {
+        console.log("4 is called for number: " + i);
+        console.log(i);
+        continue;
+    }
+    if (i & 2 === 0 && i % 4 === 0) {
+        console.log("1 is called for number: " + i);
+        result += i;
+        continue;
+    }
+    if (i % 3 === 0) {
+        console.log("2 is called for number: " + i);
+        result -= i;
+        continue;
+    }
+    if (i % 4 === 0) {
+        console.log("3 is called for number: " + i);
+        result *= i;
+        break;
+    }
+}
+
+console.log(result);
