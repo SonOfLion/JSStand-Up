94424ae1a38cd7a722c50e28c960fd6b908f7412
SonOfLion <syplusplus@gmail.com>
2020-05-13 11:23:50 +0300

    2

--- JSLoop/index.js
+++ JSLoop/index.js
@@ -5,22 +5,23 @@ let result = 0;
 
 for (let i = m; i <= n; i++) {
     if (i % 5 === 0) {
-        // console.log("4 is called for number: " + i);
+        // console.log("3 if is called")
         console.log(i);
         continue;
     }
     if (i % 2 === 0 && i % 4 !== 0) {
-        // console.log("1 is called for number: " + i);
+        // console.log("1 if is called " + i);
         result += i;
         continue;
     }
     if (i % 3 === 0) {
-        // console.log("2 is called for number: " + i);
+        // console.log("2 if is called " + i);
         result -= i;
         continue;
     }
+
     if (i % 4 === 0) {
-        // console.log("3 is called for number: " + i);
+        // console.log("3 if is called " + i);
         result *= i;
         continue;
     }
